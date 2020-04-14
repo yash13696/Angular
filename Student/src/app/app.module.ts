808ca3d300b7de3bb366614f79a3f85c0d4f9262
yash13696 <yash13696@gmail.com>
2020-04-14 15:21:14 +0530

    navigation bar completed

--- Student/src/app/app.module.ts
+++ Student/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ViewComponent } from './view/view.component';
+import { AddComponent } from './add/add.component';
+import { DeleteComponent } from './delete/delete.component';
+import { UpdateComponent } from './update/update.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ViewComponent,
+    AddComponent,
+    DeleteComponent,
+    UpdateComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
